0729d1d09576a7b7de61487b29e7a688536e40e6
amaleki2 <amir.maleki.zamenjani@gmail.com>
2020-08-19 11:39:03 -0700

    update predicting with nn

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ from data.sympy_helper import plot_sdf
 
 resolution = 200
 net = UNet(n_channels=1, n_classes=1, bilinear=True)
-net.load_state_dict(torch.load("checkpoints/CP_epoch5.pth"))
+net.load_state_dict(torch.load("checkpoints/CP_epoch100.pth", map_location=torch.device('cpu')))
 net.eval()
 X = np.load("data/X_1obj.npy").astype(float).reshape((-1, 1, resolution, resolution))
 X = torch.from_numpy(X)
@@ -29,5 +29,37 @@ sdf = sdf.squeeze()
 sdf_pred = sdf_pred.squeeze()
 print(img.shape, sdf.shape, sdf_pred.shape)
 
-plot_sdf(img, sdf)
-plot_sdf(img, sdf_pred)
+import matplotlib.pyplot as plt
+
+xx = np.linspace(0, resolution, resolution)
+plt.figure(figsize=(10,10))
+plt.subplot(2,2,1)
+plt.imshow(sdf, cmap='hot')
+plt.contour(sdf, 10, colors='k')
+plt.gca().invert_yaxis()
+plt.xticks([])
+plt.yticks([])
+for plt_idx, clr in zip([30, 100, 170], ['r', 'b', 'g']):
+    plt.plot(xx, [xx[plt_idx]]*resolution, clr + '--')
+    plt.plot([xx[plt_idx]]*resolution, xx, clr + '--')
+
+# plt.show()
+plt.subplot(2, 2, 2)
+plt.imshow(sdf_pred, cmap='hot')
+plt.contour(sdf_pred, 10, colors='k')
+plt.gca().invert_yaxis()
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(2,2,3)
+for plt_idx, clr in zip([30, 100, 170], ['r', 'b', 'g']):
+    plt.plot(xx, sdf[plt_idx, :], clr + '--')
+    plt.plot(xx, sdf_pred[plt_idx, :], clr + '-')
+
+plt.subplot(2,2,4)
+for plt_idx, clr in zip([30, 100, 170], ['r', 'b', 'g']):
+    plt.plot(sdf[:, plt_idx], xx, clr + '--')
+    plt.plot(sdf_pred[:, plt_idx], xx, clr + '-')
+
+
+plt.show()
